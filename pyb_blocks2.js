2a8ec62cfec0f4e01165d0e768f078edbc939505
kebrida <77302938+kebrida@users.noreply.github.com>
2021-01-11 14:47:31 -0800

    Create pyb_blocks2.js
    2nd

---
+++ pyb_blocks2.js
@@ -0,0 +1,39 @@
+function message(msg)
+{
+    console.log(msg);
+    console.log("from kebrida rep");
+    
+
+}
+
+var pyBoardToolBox={
+		"kind": "flyoutToolbox",
+		"contents": [			
+			{
+				"kind": "block",
+				"type": "math_number",
+				"blockxml": "<block type='math_number'><field name='NUM'>0</field></block>"
+			},
+		   
+			{
+				"kind": "block",
+				"type": "text"
+			},			
+			{
+				"kind": "block",
+				"type": "logic_compare"
+			},
+			{
+				"kind": "block",
+				"type": "controls_repeat_ext"
+			},
+			{
+				"kind": "block",
+				"type": "controls_if"
+			},
+			{
+				"kind": "block",
+				"type": "controls_whileUntil"
+			}
+		]
+	  };
